2a43f58a1923167f3c132e1a67ab0e1e8f3318d1
Branislava Sandrih <branislava.sandrih@gmail.com>
2018-01-07 11:49:33 +0100

    AUC score

--- main.py
+++ main.py
@@ -2,66 +2,64 @@
 # -*- coding: utf-8 -*-
 
 import sys
-import time
 
-import matplotlib.pylab as plt
 import numpy as np
 import pandas as pd
-from sklearn import model_selection, metrics
-from sklearn.ensemble import GradientBoostingClassifier, RandomForestClassifier
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import classification_report, confusion_matrix
-from sklearn.model_selection import train_test_split
-from sklearn.neural_network import MLPClassifier
-from sklearn.svm import LinearSVC
+from sklearn import model_selection
+from sklearn.ensemble import GradientBoostingClassifier
 
-def batch_classify(X_train, Y_train, X_test, Y_test, verbose = True):
+from scipy import interp
+import matplotlib.pyplot as plt
+from sklearn.metrics import roc_curve, auc
+from sklearn.model_selection import StratifiedKFold
 
-    df_results = pd.DataFrame(data=np.zeros(shape=(no_classifiers, 4)), columns=['classifier', 'train_score', 'test_score', 'training_time'])
-    count = 0
-    for key, classifier in dict_classifiers.items():
-        t_start = time.clock()
-        classifier.fit(X_train, Y_train)
-        t_end = time.clock()
-        t_diff = t_end - t_start
-        train_score = classifier.score(X_train, Y_train)
-        test_score = classifier.score(X_test, Y_test)
-        df_results.loc[count, 'classifier'] = key
-        df_results.loc[count, 'train_score'] = train_score
-        df_results.loc[count, 'test_score'] = test_score
-        df_results.loc[count, 'training_time'] = t_diff
-        if verbose:
-            print("trained {c} in {f:.2f} s".format(c=key, f=t_diff))
-        count += 1
-    return df_results
+# Run classifier with cross-validation and plot ROC curves
+def plot_roc(classifier, X, y, k, predictors):
+    cv = StratifiedKFold(n_splits=k)
 
-def modelfit(alg, X_train, y_train, predictors, performCV=True, printFeatureImportance=True, cv_folds=5):
+    tprs = []
+    aucs = []
+    mean_fpr = np.linspace(0, 1, 100)
 
-    # fit the algorithm on the data
-    alg.fit(X_train, y_train)
-        
-    # predict training set:
-    dtrain_predictions = alg.predict(X_train)
-    dtrain_predprob = alg.predict_proba(X_train)[:, 1]
+    i = 0
+    for train, test in cv.split(X, y):
+        probas_ = classifier.fit(X[train], y[train]).predict_proba(X[test])
 
-    # perform cross-validation:
-    if performCV:
-        cv_score = model_selection.cross_val_score(alg, X_train, y_train, cv=cv_folds, scoring='roc_auc')
-    
-    # print model report:
-    print("\nModel Report")
-    print("Accuracy : %.4g" % metrics.accuracy_score(y_train, dtrain_predictions))
-    print("AUC Score (Train): %f" % metrics.roc_auc_score(y_train, dtrain_predprob))
-    
-    if performCV:
-        print("CV Score : Mean - %.7g | Std - %.7g | Min - %.7g | Max - %.7g" % (np.mean(cv_score), np.std(cv_score), np.min(cv_score), np.max(cv_score)))
-        
-    # print feature importance:
-    if printFeatureImportance:
-        feat_imp = pd.Series(alg.feature_importances_, predictors).sort_values(ascending=False)[:15]
-        feat_imp.plot(kind='bar', title='Feature Importances')
-        plt.ylabel('Feature Importance Score')
-        plt.show()
+        if i == 0:
+            feat_imp = pd.Series(classifier.feature_importances_, predictors).sort_values(ascending=False)[:15]
+            feat_imp.plot(kind='bar', title='Feature Importances')
+            plt.ylabel('Feature Importance Score')
+            plt.tight_layout()
+            plt.show()
+
+        # Compute ROC curve and area the curve
+        fpr, tpr, thresholds = roc_curve(y[test], probas_[:, 1])
+        tprs.append(interp(mean_fpr, fpr, tpr))
+        tprs[-1][0] = 0.0
+        roc_auc = auc(fpr, tpr)
+        aucs.append(roc_auc)
+        plt.plot(fpr, tpr, lw=1, alpha=0.3,
+                 label='ROC fold %d (AUC = %0.2f)' % (i, roc_auc))
+        i += 1
+
+    plt.plot([0, 1], [0, 1], linestyle='--', lw=2, color='r', label='Luck', alpha=.8)
+    mean_tpr = np.mean(tprs, axis=0)
+    mean_tpr[-1] = 1.0
+    mean_auc = auc(mean_fpr, mean_tpr)
+    std_auc = np.std(aucs)
+    plt.plot(mean_fpr, mean_tpr, color='b', label=r'Mean ROC (AUC = %0.4f $\pm$ %0.4f)' % (mean_auc, std_auc), lw=2,
+             alpha=.8)
+    std_tpr = np.std(tprs, axis=0)
+    tprs_upper = np.minimum(mean_tpr + std_tpr, 1)
+    tprs_lower = np.maximum(mean_tpr - std_tpr, 0)
+    plt.fill_between(mean_fpr, tprs_lower, tprs_upper, color='grey', alpha=.2, label=r'$\pm$ 1 std. dev.')
+    plt.xlim([-0.05, 1.05])
+    plt.ylim([-0.05, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver Operating Characteristic curve')
+    plt.legend(loc="lower right")
+    plt.show()
 
 if __name__ == '__main__':
     
@@ -69,39 +67,23 @@ if __name__ == '__main__':
         print('python3.5 main.py dataset/data-sms-reg.csv')
         print('python3.5 main.py dataset/data-sms-form.csv')
         exit(1)
-    
+
+    # dataset
     df = pd.read_csv(sys.argv[1])
+
+    # samples and labels
     y = df['class']
     X = df.ix[:, df.columns != 'class']
 
-    # train/test sets
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
-    print('Before feature selection ', np.shape(X_train), np.shape(y_train))
-    
-    # creating model
-    clf = GradientBoostingClassifier()
-    
-    # perform feature selection
-    predictors = np.array(X.columns.values)
-    
-    # fitting model
-    clf.fit(X_train, y_train)
-    modelfit(clf, X, y, predictors)
-    
-    # predicting labels
-    y_pred = clf.predict(X_test)
-    print(classification_report(y_test, y_pred))
-    print(confusion_matrix(y_test, y_pred))
-    
-    # trying out different classifiers
-    dict_classifiers = {
-        "Logistic Regression": LogisticRegression(),
-        "Linear SVM": LinearSVC(C=0.1, penalty='l1', dual=False),
-        "Gradient Boosting Classifier": clf,
-        "Random Forest": RandomForestClassifier(n_estimators = 18),
-        "Neural Net": MLPClassifier(alpha = 1),
-    }
-    no_classifiers = len(dict_classifiers.keys())
-    
-    df_results = batch_classify(X_train, y_train, X_test, y_test)
-    print(df_results.sort_values(by='test_score', ascending=False))
+    # creating classification model
+    classifier = GradientBoostingClassifier(learning_rate=0.1, n_estimators=160, min_samples_split=10, min_samples_leaf=30, max_depth=9, max_features=11, subsample=0.8, random_state=10)
+
+    # number of folds
+    k = 5
+
+    # CV score
+    scores = model_selection.cross_val_score(classifier, X, y, cv=k, scoring='roc_auc')
+    print("CV Score : Mean - %.7g | Std - %.7g | Min - %.7g | Max - %.7g" % (np.mean(scores), np.std(scores), np.min(scores), np.max(scores)))
+
+    # plotting ROC curve
+    plot_roc(classifier, X.as_matrix(), np.array(y), k, np.array(X.columns.values))
